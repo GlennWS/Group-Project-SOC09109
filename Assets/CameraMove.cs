6d7e15ab09d33d8b1d1520681dd8e5973cd3dd0f
RebelCrow <Callumrgalbraith@gmail.com>
2018-02-08 15:05:06 +0000

    Intial-commit
    ragdoll Demo intial commit

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    GameObject playerObj;
+    Vector3 cammeraOffSet;
+	// Use this for initialization
+	void Start () {
+        playerObj = GameObject.Find("Ragdoll/Group/Main/DeformationSystem/Root_M/Spine1_M");
+        cammeraOffSet = new Vector3(-1, 5, -5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerObj.transform.position + cammeraOffSet;
+	}
+}
