821f21cbc9a3dfc980f7dafb3436c28f963b4a7e
hmacleod18 <hectormacleod17@gmail.com>
2018-04-06 06:18:37 +0100

    Add files via upload

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+	public float jump;
+    public float MaxSpeed = 100f;
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+	bool flag = false;
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+        
+}
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+		//rb2d.gravityScale = 1;
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		//rigidbody.angularVelocity *= friction
+		if (Input.GetAxis ("Horizontal") != 0f) {
+			rb2d.drag = 1;
+			rb2d.AddForce (movement * speed);
+            
+        } else if (Input.GetAxis ("Vertical") != 0f) {
+			rb2d.drag = 0;
+			rb2d.AddForce (movement * speed);
+		} else if (Input.GetKeyDown (KeyCode.Space)) {
+			flag = true;
+			//rb2d.gravityScale = 0;
+			rb2d.AddForce (Vector3.up * jump * 10);
+		} else {
+			rb2d.drag = 0;
+			rb2d.AddForce(movement * speed);
+		}
+        rb2d.velocity = Vector3.ClampMagnitude(rb2d.velocity, MaxSpeed);
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+
+    }
+}
